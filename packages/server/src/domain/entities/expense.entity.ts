8e7e14c95f674940b9611dca591266075ec5c6e0
Fábio Henrique <fabiohenriquesf8@gmail.com>
2026-02-22 12:34:15 -0300

    feat [backend]: adiciona entidades principais

---
+++ packages/server/src/domain/entities/expense.entity.ts
@@ -0,0 +1,16 @@
+﻿export enum ExpenseTypeEnum {
+  FIXED = "fixed",
+  VARIABLE = "variable",
+}
+
+export interface IExpenseEntity {
+  id: number;
+  userId: number;
+  name: string;
+  amount: number;
+  category: string;
+  type: ExpenseTypeEnum;
+  createdAt: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}
---
+++ packages/server/src/domain/entities/user.entity.ts
@@ -0,0 +1,9 @@
+﻿interface IUserEntity {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}
