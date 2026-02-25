6ae70c211dd6d10be7a9484c7b4b25cf48a2ad83
Fábio Henrique <fabiohenriquesf8@gmail.com>
2026-02-24 21:06:38 -0300

    first commit

---
+++ packages/server/src/infra/db/schemas/expense.ts
@@ -0,0 +1,33 @@
+﻿import {
+  decimal,
+  index,
+  integer,
+  pgEnum,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { ExpenseTypeEnum } from "../../../domain/entities/expense.entity";
+
+export const expenseType = pgEnum("expense_type", [
+  ExpenseTypeEnum.FIXED,
+  ExpenseTypeEnum.VARIABLE,
+]);
+
+const expenseSchema = pgTable(
+  "expenses",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer().notNull(),
+    name: varchar({ length: 100 }).notNull(),
+    amount: decimal({ precision: 10, scale: 2 }).notNull(),
+    category: varchar({ length: 100 }).notNull(),
+    type: expenseType("type").notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp(),
+    deletedAt: timestamp(),
+  },
+  (table) => [index("idx_expense_user_id").on(table.userId)],
+);
+
+export default expenseSchema;
---
+++ packages/server/src/infra/db/schemas/user.ts
@@ -0,0 +1,23 @@
+﻿import {
+  index,
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+const userSchema = pgTable(
+  "users",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 100 }).notNull(),
+    email: varchar({ length: 100 }).notNull().unique(),
+    password: varchar({ length: 100 }).notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp(),
+    deletedAt: timestamp(),
+  },
+  (table) => [index("idx_user_email").on(table.email)],
+);
+
+export default userSchema;
